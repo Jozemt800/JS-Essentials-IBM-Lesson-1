e4c01b17667bf543fd5471a58ad7fc573996d06a
“Joseph <jozemt800@gmail.com>
2025-05-15 08:58:38 -0400

    WIth Expense Tracker Exercise

--- SampleFolder/calculateArea/calculate_Area.js
+++ SampleFolder/calculateArea/calculate_Area.js
@@ -6,5 +6,21 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
 
 let area = length * width;
+ document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
 
+
+
+let montlhyRental;
+let transportCost;
+let electricity;
+
+
+function calculateTotal () {
+    montlhyRental = parseFloat(document.getElementById('monthlyRental').value);
+    transportCost = parseFloat(document.getElementById('transportCost').value);
+    electricity = parseFloat(document.getElementById('electricity').value);
+
+    let bill = montlhyRental + transportCost + electricity;
+        document.getElementById('bill').innerText = `Your Monthly Expenses Total is: K ${bill}`;
 }
